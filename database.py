45a43e98bea2d08222ad9fb39be0a24df8a67190
Metra <metra-2004@mail.ru>
2025-07-02 01:06:25 +0500

    3

--- database.py
+++ database.py
@@ -1,12 +1,20 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+asyncmy://admin:admin@localhost:3306/mynewdb"
+# Загрузить .env
+load_dotenv()
 
+# Получить URL из .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Создать асинхронный движок
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
+# Dependency для FastAPI
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
