02c393add168df01a08ca2f9fd3b28ad8a941614
Metra <metra-2004@mail.ru>
2025-07-02 18:06:57 +0500

    3

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.on_event("startup")
